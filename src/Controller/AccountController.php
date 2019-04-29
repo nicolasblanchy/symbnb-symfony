de9baa9139d13ede69ae5bd5b794c5bb4a1cae21
Nicolas Blanchy <blanchynicolas@gmail.com>
2019-04-29 10:49:46 +0200

    Gestion des réservations

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -168,4 +168,15 @@ class AccountController extends AbstractController
             'user' =>$this->getUser()
         ]);
     }
+
+    /**
+     * Permet d'afficher la liste des réservations faites par l'utilisateur
+     * 
+     * @Route("/account/bookings", name="account_bookings")
+     *
+     * @return Reponse
+     */
+    public function bookings(){
+        return $this->render('account/bookings.html.twig');
+    }
 }
